53da3844a7e74febb6e8cc8908ad351521b37247
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-06-16 17:34:09 -0500

    update python packaging

--- python/gritql/installer.py
+++ python/gritql/installer.py
@@ -2,15 +2,14 @@ from __future__ import annotations
 
 import os
 import sys
-import json
 import shutil
 import tarfile
 import platform
 
-from typing import TYPE_CHECKING, List
 from pathlib import Path
 
 import httpx
+
 
 def _cache_dir() -> Path:
     xdg = os.environ.get("XDG_CACHE_HOME")
@@ -24,23 +23,27 @@ def _debug(message: str) -> None:
     if not os.environ.get("DEBUG"):
         return
 
-    sys.stdout.write(f"[DEBUG]: {message}\n")
+    sys.stderr.write(f"[DEBUG]: {message}\n")
 
 
 class CLIError(Exception):
     pass
 
+
 def find_install() -> Path:
     """Installs the Grit CLI and returns the location of the binary"""
-    if sys.platform == "win32":
-        raise CLIError("Windows is not supported yet in the migration CLI")
-
     grit_path = shutil.which("grit")
     if grit_path:
         _debug(f"'grit' found in PATH at {grit_path}")
         return Path(grit_path)
 
-    platform = "apple-darwin" if sys.platform == "darwin" else "unknown-linux-gnu"
+    platform = (
+        "apple-darwin"
+        if sys.platform == "darwin"
+        else "pc-windows-msvc"
+        if sys.platform == "win32"
+        else "unknown-linux-gnu"
+    )
 
     dir_name = _cache_dir() / "grit"
     install_dir = dir_name / ".install"
@@ -68,7 +71,9 @@ def find_install() -> Path:
     _debug(f"Using architecture {arch}")
 
     file_name = f"grit-{arch}-{platform}"
-    download_url = f"https://github.com/getgrit/gritql/releases/latest/download/{file_name}.tar.gz"
+    download_url = (
+        f"https://github.com/getgrit/gritql/releases/latest/download/{file_name}.tar.gz"
+    )
 
     sys.stdout.write(f"Downloading Grit CLI from {download_url}\n")
     with httpx.Client() as client:
@@ -98,12 +103,14 @@ def find_install() -> Path:
 
     return target_path
 
+
 def _move_files_recursively(source_dir: Path, target_dir: Path) -> None:
     for item in source_dir.iterdir():
         if item.is_file():
             item.rename(target_dir / item.name)
         elif item.is_dir():
             _move_files_recursively(item, target_dir)
+
 
 def _get_arch() -> str:
     architecture = platform.machine().lower()
--- python/gritql/run.py
+++ python/gritql/run.py
@@ -1,26 +1,31 @@
+from __future__ import annotations
+
 import subprocess
-from typing import Any
 import sys
+
+from typing import Sequence
 
 from .installer import find_install
 
-def run_cli(args: Any):
+
+def run_cli(args: Sequence[str]) -> int:
     """Runs the Grit CLI"""
     cli_path = find_install()
-    print("Running GritQL pattern with args:", cli_path, args)
-
-    code = subprocess.run([cli_path, *args])
-
-
+    print("Running GritQL pattern with args:", cli_path, *args, file=sys.stderr)
+    code = subprocess.run([str(cli_path), *args])
     return code.returncode
 
-def apply_pattern(pattern_or_name: str, args: Any, grit_dir: str = None):
+
+def apply_pattern(
+    pattern_or_name: str, args: Sequence[str], grit_dir: str | None = None
+) -> int:
     """Applies a GritQL pattern to the Grit CLI"""
     final_args = ["apply", pattern_or_name, *args]
     if grit_dir:
         final_args.append("--grit-dir")
         final_args.append(grit_dir)
     return run_cli(final_args)
+
 
 if __name__ == "__main__":
     run_cli(sys.argv[1:])
